8089cc8ee6e60d043ae45b6732ccdff1095cb2be
James McKinney <james@slashpoundbang.com>
2013-05-31 14:37:53 -0400

    remove unneeded credential helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,19 +5,3 @@ Coveralls.wear!
 
 require 'rspec'
 require File.dirname(__FILE__) + '/../lib/popit'
-
-def credentials_file
-  File.expand_path(File.dirname(__FILE__) + '/spec_auth.yml')
-end
-
-def credentials?
-  File.exist?(credentials_file)
-end
-
-def credentials
-  if credentials?
-    YAML.load_file(credentials_file)
-  else
-    {:instance_name => 'tttest'}
-  end
-end
